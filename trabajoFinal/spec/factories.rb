4ffd68266b3e3dacbe625db36a572cebca2c735b
pochy4545 <agustin.c.96@hotmail.com>
2018-12-10 20:11:55 -0300

    test

--- trabajoFinal/spec/factories.rb
+++ trabajoFinal/spec/factories.rb
@@ -1,9 +1,47 @@
 FactoryBot.define do
-  
-  factory :user do
-    username {"agustin"}
-    email {"agustin.c.96@hotmail.com"}
-    screen_name {"pochy4545"}
-    password {"mierda4545"}
-  end
+ factory :user do
+ 	username {'agustin'}
+ 	email { Faker::Internet.unique.email}
+ 	screen_name {'pochy4545'}
+ 	password {'password'}
+ 	token { Faker::Number.number(10) }
+
+ 	transient do 
+ 		questions_count {1}
+ 	end
+
+ 	after(:create) do |user, evaluator|
+ 		create_list(
+ 			:question,
+ 			evaluator.questions_count,
+ 			user: user
+ 			)
+ 	end
+ end
+
+ factory :question do
+ 	title {Faker::Lorem.setence}
+ 	description {Faker::Lorem.paragraph}
+ 	status {false}
+ 	answer {factory :answer}
+ 	user {factory :user}
+
+ 	transient do
+ 		answer_count {2}
+ 	end
+
+ 	after(:create) do |question,evaluator|
+ 		create_list(
+ 			:answer,
+ 			evaluator.answer_count,
+ 			question: question
+ 			)
+ 	end
+ end
+
+ factory :answer do
+ 	content {Faker::Lorem.sentence}
+ 	question {factory :question}
+ 	user {factory :user}
+ end
 end
--- trabajoFinal/spec/model/user_spec.rb
+++ trabajoFinal/spec/model/user_spec.rb
@@ -7,32 +7,26 @@ RSpec.describe User, :type => :model do
   
   
   it "username tiene que ser unico" do
-    user2 = build(:user, username: "agustin")
-    expect(user2).to_not be_valid
+    
   end
   
   it "email tiene que ser unico" do
-    user2 = build(:user, email: "agustin.c.96@hotmail.com")
-    expect(user2).to_not be_valid
+    
   end
   
   it "password invalido" do 
-    user2 = build(:user, password: nil)
-    expect(user2).to_not be_valid
+    
   end
   
   it "username invalido" do 
-    user2 = build(:user, username: nil)
-    expect(user2).to_not be_valid
+    
   end
   
   it "email invalido" do
-    user2 = build(:user, email: nil)
-    expect(user2).to_not be_valid
+    
   end
   it "screen_name invalido" do
-    user2 = build(:user, screen_name: nil)
-    expect(user2).to_not be_valid
+    
   end
   
 end
