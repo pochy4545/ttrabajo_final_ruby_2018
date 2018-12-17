52db82107acafc8db29daade0329ca25805a9497
pochy4545 <agustin.c.96@hotmail.com>
2018-12-16 21:35:07 -0300

    se creeo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-4.times do |i|
-  User.create(username: "username#{i}",email: "user#{i}@example.com", password: "1234#{i}", screen_name: "screen_name#{i}")
-end
 
+ c1 = User.create(username: "genetfirst",email: "genetest1@example.com", password: "1234", screen_name: "genetestfirst" , token: nil)
+ c2 = User.create(username: "genesecond",email: "genetest2@example.com", password: "1234", screen_name: "genetestsecond", token: nil)
+ 
+ q1 = Question.create( title: "genetest1", description:"undefined1", status:false, answer_id: nil, user_id: c1.id )
+ q2 = Question.create( title: "genetest2", description:"undefined2", status:false, answer_id: nil, user_id: c2.id )
+
+4.times do |i|
+ a1 = Answer.create( content: "genetest#{i}" , question_id: q1.id , user_id: c1.id)
+end
